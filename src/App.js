aa1ce9a67be51afb5adaea3d4010e6c5a6a93c17
Michael Fuget <michaelfuget@gmail.com>
2021-03-12 02:16:42 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {CatComponent} from './CatComponent';
-import {GraceHopperQuoteComponent} from './CatComponent';
+import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent';
 
 class App extends Component {
 	render() {
